612d221fea4e7f9a90392db8ba4f7e09892dbf05
gabriel <gr9981286@gmail.com>
2024-12-08 20:43:04 -0400

    adicionando myenv no gitignore

--- app.py
+++ app.py
@@ -3,25 +3,21 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Função para validar a senha
 def validar_senha(senha):
-    # Expressão regular para validar a senha
     if len(senha) < 6:
         return False, "A senha deve ter pelo menos 6 caracteres."
-    if not re.search(r'[A-Z]', senha):  # Verifica se tem pelo menos uma letra maiúscula
+    if not re.search(r'[A-Z]', senha):
         return False, "A senha deve conter pelo menos uma letra maiúscula."
-    if not re.search(r'[0-9]', senha):  # Verifica se tem pelo menos um número
+    if not re.search(r'[0-9]', senha):
         return False, "A senha deve conter pelo menos um número."
-    if not re.search(r'[\W_]', senha):  # Verifica se tem pelo menos um caractere especial
+    if not re.search(r'[\W_]', senha):
         return False, "A senha deve conter pelo menos um caractere especial."   
     return True, ""
 
-# Página inicial (Login)
 @app.route('/')
 def login():
     return render_template('login.html')
 
-# Página de Cadastro
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -33,16 +29,13 @@ def register():
         senha = request.form['senha']
         confirmacao_senha = request.form['confirmacao_senha']
         
-        # Verifica se as senhas coincidem
         if senha != confirmacao_senha:
             return "<script>alert('Senhas não coincidem!');window.history.back();</script>"
         
-        # Valida a senha
         is_valid, message = validar_senha(senha)
         if not is_valid:
             return f"<script>alert('{message}');window.history.back();</script>"
         
-        # Aqui você pode adicionar lógica para salvar os dados no banco de dados ou outra lógica.
         return redirect(url_for('login'))
     
     return render_template('register.html')
